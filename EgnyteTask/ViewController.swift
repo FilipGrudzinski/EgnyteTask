32a2ab0672a002b9aebd7bbc8544df9cd2d3c58c
FilipGrudzinski <grudzinski.filip@gmail.com>
2019-05-02 14:08:28 +0200

    IBOutlet and IBAction added

--- EgnyteTask/ViewController.swift
+++ EgnyteTask/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var resultsLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func downloadWeatherButton(_ sender: Any) {
+        print("downloadWeatherButton")
+    }
+    @IBAction func printWeatherButton(_ sender: Any) {
+        print("printWeatherButton")
+    }
+    @IBAction func downloadFootBallButton(_ sender: Any) {
+        print("downloadFootBallButton")
+    }
+    @IBAction func printFootBallButton(_ sender: Any) {
+        print("printFootBallButton")
+    }
 }
 
