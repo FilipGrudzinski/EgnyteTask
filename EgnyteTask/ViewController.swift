f8435da24ac67931faf761ea9b8df93e7ca0c247
FilipGrudzinski <grudzinski.filip@gmail.com>
2019-05-03 20:23:41 +0200

    popup fixes prints dat file

--- EgnyteTask/ViewController.swift
+++ EgnyteTask/ViewController.swift
@@ -18,15 +18,24 @@ class ViewController: UIViewController {
     }
     
     @IBAction func downloadWeatherButton(_ sender: Any) {
-        print("downloadWeatherButton")
         downloadFileFromURL("weather.dat", weatherUrl)
     }
     
     @IBAction func printWeatherButton(_ sender: Any) {
-        print("printWeatherButton")
+        let documentsUrl:URL = (FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first as URL?)!
+        let path = documentsUrl.appendingPathComponent("football.dat").path
+        do {
+            // Read an entire text file into an NSString.
+            let contents = try? NSString(contentsOfFile: path,
+                                         encoding: String.Encoding.ascii.rawValue)
+            print(contents!)
+            // Print all lines.
+            contents!.enumerateLines({ (line, stop) -> () in
+                print("Line = \(line)")
+            })
+        }
     }
     @IBAction func downloadFootBallButton(_ sender: Any) {
-        print("downloadFootBallButton")
         downloadFileFromURL("football.dat", footballUrl)
     }
     @IBAction func printFootBallButton(_ sender: Any) {
@@ -44,17 +53,17 @@ class ViewController: UIViewController {
         let request = URLRequest(url:fileURL!)
         
         if FileManager.default.fileExists(atPath: destinationFileUrl.path) {
-            popUp(fileName, url, destinationFileUrl)
+            replacePopUp(fileName, url, destinationFileUrl)
         } else {
             let task = session.downloadTask(with: request) { (tempLocalUrl, response, error) in
                 if let tempLocalUrl = tempLocalUrl, error == nil {
-                    // Success
                     if let statusCode = (response as? HTTPURLResponse)?.statusCode {
                         print("Successfully downloaded. Status code: \(statusCode)")
+                        self.saveItem(tempLocalUrl, destinationFileUrl)
                     }
-                    self.saveItem(tempLocalUrl, destinationFileUrl)
                 } else {
-                    print("Error took place while downloading a file. Error description: %@", error?.localizedDescription ?? "");
+                    print("Error took place while downloading a file. Error description: %@", error?.localizedDescription ?? "")
+                    self.successPopUp("Try again later")
                 }
             }
             task.resume()
@@ -64,31 +73,44 @@ class ViewController: UIViewController {
     private func saveItem(_ tempLocalUrl: URL, _ destinationFileUrl: URL) {
         do {
             try FileManager.default.copyItem(at: tempLocalUrl, to: destinationFileUrl)
-            print("Successfully saved")
+            successPopUp("Successfully saved")
         } catch (let writeError) {
             print("Error creating a file \(destinationFileUrl) : \(writeError)")
         }
     }
     
-    private func popUp(_ fileName: String, _ url: String, _ destinationFileUrl: URL)  {
-        let alert = UIAlertController(title: "Warning", message: "File Exist Replace", preferredStyle: .alert)
-        let yesAction = UIAlertAction(title: "Yes", style: .default) {(UIAlertAction) in self.replaceExistingFile(fileName, url, destinationFileUrl) }
-        let noAction = UIAlertAction(title: "No", style: .cancel) {(UIAlertAction) in self.dismissPopUp()}
-        
-        alert.addAction(yesAction)
-        alert.addAction(noAction)
-        
-        present(alert, animated: true, completion: nil)
+    private func successPopUp(_ message: String) {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: message, message: "", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Ok", style: .cancel) {(UIAlertAction) in self.dismissPopUp()}
+            
+            alert.addAction(okAction)
+            
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    private func replacePopUp(_ fileName: String, _ url: String, _ destinationFileUrl: URL)  {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: "File Exist", message: "Replace?", preferredStyle: .alert)
+            let yesAction = UIAlertAction(title: "Yes", style: .default) {(UIAlertAction) in self.replaceExistingFile(fileName, url, destinationFileUrl) }
+            let noAction = UIAlertAction(title: "No", style: .cancel) {(UIAlertAction) in self.dismissPopUp()}
+            
+            alert.addAction(yesAction)
+            alert.addAction(noAction)
+            
+            self.present(alert, animated: true, completion: nil)
+        }
     }
     
     private func replaceExistingFile(_ fileName: String, _ url: String, _ destinationFileUrl: URL){
         do {
             try FileManager.default.removeItem(at: destinationFileUrl)
-            print("Successfully removed")
+            self.downloadFileFromURL(fileName, url)
         } catch (let writeError) {
-            print("Error creating a file \(destinationFileUrl) : \(writeError)")
+            print("Error removing a file \(destinationFileUrl) : \(writeError)")
         }
-        downloadFileFromURL(fileName, url)
+        dismissPopUp()
     }
     
     private func dismissPopUp() {
